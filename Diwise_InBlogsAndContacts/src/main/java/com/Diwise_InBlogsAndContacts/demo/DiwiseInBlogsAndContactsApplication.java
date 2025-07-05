7cd87b9b6fb3d56c14493ff5630877962316779d
Prajwal <prajwalkulkarni169@gmail.com>
2025-07-05 17:31:53 +0530

    initail commit by Psk

---
+++ Diwise_InBlogsAndContacts/src/main/java/com/Diwise_InBlogsAndContacts/demo/DiwiseInBlogsAndContactsApplication.java
@@ -0,0 +1,13 @@
+package com.Diwise_InBlogsAndContacts.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiwiseInBlogsAndContactsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiwiseInBlogsAndContactsApplication.class, args);
+	}
+
+}
---
+++ Diwise_InBlogsAndContacts/src/test/java/com/Diwise_InBlogsAndContacts/demo/DiwiseInBlogsAndContactsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Diwise_InBlogsAndContacts.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiwiseInBlogsAndContactsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
